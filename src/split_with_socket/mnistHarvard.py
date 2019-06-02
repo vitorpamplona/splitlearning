a2296510600f598c39e5d2fb05562701500c7ade
Vitor Pamplona <vitor@vitorpamplona.com>
2019-06-02 15:04:46 -0400

    Adding options for nebula and aws servers.

--- src/split_with_socket/mnistHarvard.py
+++ src/split_with_socket/mnistHarvard.py
@@ -11,7 +11,9 @@ from torchvision import datasets, transforms
 from torch import nn, optim
 from torch.autograd import Variable
 
-relay_server_host = '127.0.0.1'  # as both code is running on same pc
+#relay_server_host = 'nebula.media.mit.edu'  # as both code is running on same pc
+relay_server_host = 'ec2-3-14-72-103.us-east-2.compute.amazonaws.com'
+#relay_server_host = '127.0.0.1' 
 relay_server_port = 5004  # socket server port number
 
 client_id = 'HARVARD'
