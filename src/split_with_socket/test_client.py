ede2be83a5a36cfe0029f7ca0c0cf09573de11df
Vitor Pamplona <vitor@vitorpamplona.com>
2019-06-02 14:57:17 -0400

    Adding a testing client to evaluate new servers

--- src/split_with_socket/test_client.py
+++ src/split_with_socket/test_client.py
@@ -1,12 +1,13 @@
 import utils.client as client
 
 #relay_server_host = 'nebula.media.mit.edu'  # as both code is running on same pc
-relay_server_host = 'ec2-3-14-72-103.us-east-2.compute.amazonaws.com'
-#relay_server_host = '127.0.0.1' 
+#relay_server_host = 'ec2-3-14-72-103.us-east-2.compute.amazonaws.com'
+relay_server_host = '127.0.0.1' 
 relay_server_port = 5004  # socket server port number
 
 client_id = 'ME'
 client_send_to = 'ME'
+test_data = 'Hello'
 
 class DataPackage():
     def __init__(self, s):
@@ -15,8 +16,11 @@ class DataPackage():
 
 client.connect(relay_server_host, relay_server_port, client_id)
 
-client.sendData("ME", DataPackage("TEST"))
-print(client.receiveData().s)
+print('Testing sending the word ' + test_data)
+
+client.sendData(client_send_to, DataPackage(test_data))
+
+print('Received back the word: ' + client.receiveData().s)
 
 client.disconnect()  # close the connection
 
