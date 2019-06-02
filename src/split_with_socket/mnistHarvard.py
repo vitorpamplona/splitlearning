8b314e37b555e8f52819a398d28f9eb656163948
Vitor Pamplona <vitor@vitorpamplona.com>
2019-06-02 18:52:01 -0400

    Returning to localhost

--- src/split_with_socket/mnistHarvard.py
+++ src/split_with_socket/mnistHarvard.py
@@ -12,8 +12,8 @@ from torch import nn, optim
 from torch.autograd import Variable
 
 #relay_server_host = 'nebula.media.mit.edu'  # as both code is running on same pc
-relay_server_host = 'ec2-3-14-72-103.us-east-2.compute.amazonaws.com'
-#relay_server_host = '127.0.0.1' 
+#relay_server_host = 'ec2-3-14-72-103.us-east-2.compute.amazonaws.com'
+relay_server_host = '127.0.0.1' 
 relay_server_port = 5004  # socket server port number
 
 client_id = 'HARVARD'
